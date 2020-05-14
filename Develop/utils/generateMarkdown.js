28f2b3855253b9cb477d89ed77cd8f74704304ca
Florencia Tabarez <fftabarez97@gmail.com>
2020-05-13 19:39:13 -0600

    Updating generateMarkdown.js.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,35 +1,55 @@
 function generateMarkdown(data) {
   return `
 # ${data.ProjectTitle}
+
 [![star this repo](https://githubbadges.com/star.svg?user=fftab&repo=READMEGenerator&style=default)](https://github.com/fftab/READMEGenerator)
 [![fork this repo](https://githubbadges.com/fork.svg?user=fftab&repo=READMEGenerator&style=default)](https://github.com/fftab/READMEGenerator/fork)
 ## Description
 
 ${data.ProjectDescription}
 
-## Table of Contents 
+## Table of Contents
+
 * [Installation](#installation)
+
 * [Usage](#usage)
+
 * [License](#license)
+
 * [Contributing](#contributing)
-* [Tests](#tests)
+
+* [Testing](#testing)
+
 * [Questions](#questions)
+
 ## Installation
-To install necessary dependencies, run the following command
+
+To install necessary dependencies, run the following command:
 ${data.Installation}
+
 ## Usage
+
 ${data.Usage}
+
 ## License
+
 ${data.License}
+
 ## Contributing
+
 ${data.Contributing}
-## Test
+
+## Testing
+
 To run tests, run the following command:
 ${data.Tests}
+
 ## Questions
+
 <img src="${data.avatar_url}" alt="github avatar" style="border-radius: 16px" width="30"/>
 
 If you have any questions about the repo, please contact
+
 ${data.GitHub} @ ${data.email}
 `;
 }
